9da490e31f41773ad64b4b0ab3c81b6011c6feba
fabiomagajr <fabiomagajr@gmail.com>
2025-05-18 11:52:31 -0300

    Limpeza de código

--- prjEventosWeb/Models/Inscricao.cs
+++ prjEventosWeb/Models/Inscricao.cs
@@ -2,18 +2,7 @@
 {
     public class Inscricao
     {
-        /*
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Email { get; set; }
-        public DateTime DataInscricao { get; set; }
-        public int EventoId { get; set; }
-        // Relacionamento com a tabela de Eventos
-        public Evento Evento
-        {
-            get; set;
-        }
-        */
+        
         public int Id { get; set; }
 
         public int EventoId { get; set; }
